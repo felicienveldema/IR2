ca0dc7ddfc2e03d038dbd4db62c020210cc3501e
Tychokoster <tychokoster@hotmail.com>
2018-11-28 15:11:19 +0100

    decoder done

--- model.py
+++ model.py
@@ -2,20 +2,25 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from DecoderRNN2 import DecoderRNN
-
 class TopicSeq2Seq(nn.Module):
-    def __init__(self, device, input_size, hidden_size, output_size, sos_id, eos_id, variable_lengths=True):
+    def __init__(self, device, input_size, hidden_size, output_size, sos_id, eos_id, max_length, variable_lengths=True):
         super(TopicSeq2Seq, self).__init__()
         self.variable_lengths = variable_lengths
         self.device = device
+        self.sos_id = sos_id
         self.encoder = EncoderRNN(device, input_size, hidden_size, variable_lengths)
-        self.decoder = DecoderRNN(device, hidden_size, output_size, sos_id, eos_id)
+        self.decoder = DecoderRNN(device, hidden_size, output_size, sos_id, eos_id, max_length)
 
     def forward(self, input, input_length):
         out, hidden = self.encoder.forward(input.long(), input_length)
-        out = self.decoder.forward(out.long(), hidden)
+        out = self.decoder.forward(self.construct_input_decoder(input.shape[0]), hidden)
         return out
+
+    def construct_input_decoder(self, batch_size):
+        input_decoder = []
+        for _ in range(batch_size):
+            input_decoder.append([self.sos_id])
+        return torch.LongTensor(input_decoder).to(self.device)
 
 
 class EncoderRNN(nn.Module):
@@ -37,21 +42,33 @@ class EncoderRNN(nn.Module):
         return output, hidden
 
 class DecoderRNN(nn.Module):
-    def __init__(self, device, hidden_size, output_size, sos_id, eos_id):
+    def __init__(self, device, hidden_size, output_size, sos_id, eos_id, max_length):
         super(DecoderRNN, self).__init__()
         self.device = device
         self.hidden_size = hidden_size
         self.embedding = nn.Embedding(output_size, hidden_size)
         self.gru = nn.GRU(hidden_size, hidden_size)
         self.out = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.LogSoftmax(dim=1)
+        self.softmax = nn.LogSoftmax(dim=2)
+        self.max_length = max_length
+        self.sos_id = sos_id
+        self.eos_id = eos_id
 
     def forward(self, input, hidden):
         output = self.embedding(input)
         output = F.relu(output)
-        output, hidden = self.gru(output.squeeze(0))
-        output = self.out(output)
-        output = self.softmax(output)
-        print(output.shape)
-        self.hidden = hidden
-        return output, hidden
+        sentence = []
+        decoder_output = []
+        for i in range(self.max_length):
+            out, hidden = self.gru(output, hidden)
+
+            out = self.out(out)
+
+            out = self.softmax(out)
+            word_id = out.max(2)[1]
+            sentence.append(word_id.cpu().numpy()[0][0])
+            decoder_output.append(out)
+            self.hidden = hidden
+            if word_id == self.eos_id:
+                break
+        return sentence, hidden, decoder_output
